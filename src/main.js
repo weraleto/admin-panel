555256aafab255f1e1aeb99d770b2ede22387e90
weraleto <52561921+weraleto@users.noreply.github.com>
2020-11-09 21:32:41 +0300

    delete VueInputMask

--- src/main.js
+++ src/main.js
@@ -7,9 +7,7 @@ import {router} from './router'
 import './plugins/axios.js'
 import './plugins/vee-validate.js'
 import './plugins/element-ui.js'
-const VueInputMask = require('vue-inputmask').default
 
-Vue.use(VueInputMask)
 
 // styles
 import './assets/sass/style.sass'
